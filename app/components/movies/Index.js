4d9bd0987fecd3b2e806b5376633fe6a266a0965
Mate Antičević <mate@anticevic.net>
2017-03-18 19:12:09 +0100

    PIVY-91 Movie module search box

--- app/components/movies/Index.js
+++ app/components/movies/Index.js
@@ -1,6 +1,8 @@
 import ItemsPanel from "../common/ItemsPanel"
 import TableMovies from "./TableMovies"
 import Widget from "../dashboard/Widget"
+import FilterPanel from "../common/FilterPanel"
+import Options from "../common/Options"
 
 var Movies = React.createClass({
 
@@ -27,28 +29,131 @@ var Movies = React.createClass({
         }.bind(this);		
 
 	  },
+	  onRatedHigherChange: function(rating){
+		  this.setState({ratedHigher: rating}, this.getMovies);
+	  },
+	  onRatedLowerChange: function(rating){
+		  this.setState({ratedLower: rating}, this.getMovies);
+	  },
+	  onMyRatingChange: function(myRating){
+		  this.setState({myRating: myRating}, this.getMovies);
+	  },
+	  onTitleChanged: function(e) {
+		  this.setState({title: e.target.value}, this.getMovies);
+	  },
+	  onYearChange: function(year) {
+		  this.setState({year: year}, this.getMovies);
+	  },
+	  getMovies: function() {
+
+		var query = {
+			myRating: this.state.myRating,
+			ratingHigher: this.state.ratedHigher,
+			ratingLower: this.state.ratedLower,
+			title: this.state.title,
+			year: this.state.year
+		};
+
+        api.getMovies(query).OnSuccess = function(items){
+            this.setState({movies: items});
+        }.bind(this);
+	  },
+	  getRatings: function(){
+
+		  var ratings = [];
+
+		  for(var i = 100; i > 0; i--){
+			  ratings.push({valueId: (i/10), name: (i/10).toString()});
+		  }
+
+		  return ratings;
+	  },
+	  getYears: function(){
+		  var currentYear = new Date().getFullYear();
+
+		  var years = [];
+
+		  for(var i = currentYear; i >= 1900; i--){
+			  years.push({valueId: i, name: i.toString()});
+		  }
+
+		  return years;
+	  },
+	  getMyRatings: function(){
+
+		  var myRatings = [];
+
+		  for(var i = 10; i >= 1; i--){
+			  myRatings.push({valueId: i, name: i.toString()});
+		  }
+
+		  return myRatings;
+	  },
 	  getInitialState: function() {
-		return {movies: []};
+		return {
+			movies: [],
+			myRatings: this.getMyRatings(),
+			ratings: this.getRatings(),
+			years: this.getYears(),
+			title: ""
+		};
 	  },
       render: function () {
         return (
 					<div>
 						<div className="row">
-							<div className="col-lg-3">
+							<div className="col-md-3">
 								<Widget value={this.state.totalCount} title="Total"/>
 							</div>
-							<div className="col-lg-3">
+							<div className="col-md-3">
 								<Widget value={this.state.yearCount} title="Year"/>
 							</div>
-							<div className="col-lg-3">
+							<div className="col-md-3">
 								<Widget value={this.state.monthCount} title="Month"/>
 							</div>
-							<div className="col-lg-3">
+							<div className="col-md-3">
 								<Widget value={this.state.weekCount} title="Week"/>
 							</div>
 						</div>
 						<div className="row">
-							<div className="col-lg-12">
+							<div className="col-md-3">
+								<FilterPanel title="Search">
+									<div className="row row-spacing">
+										<div className="col-md-12">
+											<input className="form-control" type="text" placeholder="Title" onChange={this.onTitleChanged} />
+										</div>
+									</div>
+									<div className="row row-spacing">
+										<div className="col-md-12">
+											<Options items={this.state.years}
+													 onChange={this.onYearChange}
+													 defaultText="Year"/>
+										</div>
+									</div>
+									<div className="row row-spacing">
+										<div className="col-md-12">
+											<Options items={this.state.myRatings}
+													 onChange={this.onMyRatingChange}
+													 defaultText="My rating"/>
+										</div>
+									</div>
+									<div className="row row-spacing">
+										<div className="col-md-12">
+											<Options items={this.state.ratings}
+													 onChange={this.onRatedHigherChange}
+													 defaultText="Rated higher"/>
+										</div>
+									</div>
+									<div className="row row-spacing">
+										<div className="col-md-12">
+											<Options items={this.state.ratings}
+													 onChange={this.onRatedLowerChange}
+													 defaultText="Rated lower"/>
+										</div>
+									</div>
+								</FilterPanel>
+							</div>
+							<div className="col-md-9">
 								<ItemsPanel title="Movies">
 									<TableMovies items={this.state.movies} />
 								</ItemsPanel>
